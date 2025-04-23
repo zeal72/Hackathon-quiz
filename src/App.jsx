9145bbfd3e6643a8415cdba9a38f7572d3fab13f
Abraham zeal <abrahamzeal72@gmail.com>
2025-04-23 01:01:05 -1100

    Bugs Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/signup" />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/quizpage" element={<QuizPage />} />
